2c4da38df93cc39b15df507a803e5097ea891603
adityashukla700 <adityashukla7002@gmail.com>
2024-03-01 09:34:27 +0000

    fix: logger function

--- src/logger.rs
+++ src/logger.rs
@@ -1,12 +1,13 @@
-use tracing_appender::non_blocking::WorkerGuard;
+use tracing_appender::{non_blocking::WorkerGuard, rolling::hourly};
+use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt};
 
-pub fn initialize_logger() -> WorkerGuard{
-    let file_appender = tracing_appender::rolling::daily("./log", "circuit_serializer.log");
-        let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
-        tracing_subscriber::fmt()
-            .with_writer(non_blocking)
-            .json()
-            .pretty()
-            .init();
+pub fn initialize_logger() -> WorkerGuard {
+
+    let file_appender = hourly("./log", "multithreaded-fs.log");
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+    let file_layer = fmt::layer().with_writer(non_blocking).json().pretty();
+
+    let stdout_log = tracing_subscriber::fmt::layer().compact();
+    tracing_subscriber::registry().with(stdout_log).with(file_layer).init();
     _guard
 }
